d7dfb02110478173ea5f62d87687c1d59b96f699
Brandon Ballentine <brandon.ballentine@gmail.com>
2016-08-31 11:34:38 -0700

    Add UI for Add Checklist view

--- MinimaList/MinimaList/AddCheckistViewController.swift
+++ MinimaList/MinimaList/AddCheckistViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class AddCheckistViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        titleLabel.becomeFirstResponder()
+        
     }
 
     override func didReceiveMemoryWarning() {
