58fd3126e4c2cfddf586f29cb897458c43f60717
Brandon Ballentine <brandon.ballentine@gmail.com>
2016-09-01 10:06:27 -0700

    Add icons for tracking progress

--- MinimaList/MinimaList/AllListsViewController.swift
+++ MinimaList/MinimaList/AllListsViewController.swift
@@ -48,7 +48,6 @@ class AllListsViewController: UITableViewController, AddChecklistViewControllerD
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         tableView.reloadData()
     }
 
@@ -110,25 +109,23 @@ class AllListsViewController: UITableViewController, AddChecklistViewControllerD
     }
     
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            dataModel.lists.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
--- MinimaList/MinimaList/ChecklistViewController.swift
+++ MinimaList/MinimaList/ChecklistViewController.swift
@@ -86,17 +86,13 @@ class ChecklistViewController: UITableViewController, AddChecklistItemViewContro
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            checklist.items.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
     /*
     // Override to support rearranging the table view.
